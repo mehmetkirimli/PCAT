30a6dca4050b6fd05b059ac3e05b6500deb0327b
mehmetkirimli <kirimlimehmet@gmail.com>
2023-08-02 22:10:25 +0300

    created express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('selam');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+   const photo = {
+      id: 1,
+      name: 'Photo Name Kartal',
+      description: 'KARA KARTAL',
+   };
+   res.send(photo);
+});
+
+app.listen(PORT, () => {
+   console.log(`Sunucu ${PORT} portunda başlatıldı.`);
+});
