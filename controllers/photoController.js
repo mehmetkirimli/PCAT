44cd854cdc976564542e7b6873ad08a0d5d7a3ac
mehmetkirimli <kirimlimehmet@gmail.com>
2023-09-03 16:50:09 +0300

    countDocuments change estimate

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 exports.getAllPhotos = async (req, res) => {
    const page = req.query.page || 1;
    const photosPerPage = 2;
-   const totalPhotos = await Photo.find().countDocuments();
+   const totalPhotos = await Photo.estimatedDocumentCount();
 
    const photos = await Photo.find({})
       .sort('createdAt')
